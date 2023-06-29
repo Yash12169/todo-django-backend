9501447caa4eb1d211ce610301b13a34fe1d0d6b
Yash <jewalkaryash@gmail.com>
2023-06-29 08:10:29 +0530

    signin/signup

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from todo_app.views import index_view
+from todo_app.views import index_view,sign_up_view,sign_in_view
 
 urlpatterns = [
     path('',index_view,name='index'),
+    path('sign-up/',sign_up_view,name='sign_up'),
+    path('sign-in/',sign_in_view,name='sign_in'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index_view(request):
-    pass
+    user=request.user
+    return render(request,'index.html')
+
+
+def sign_up_view(request):
+    return render(request,'signup.html')
+
+
+def sign_in_view(request):
+    return render(request,'signin.html')
