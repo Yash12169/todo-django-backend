20ed498aa45bb76f96bacc9e84aa4e36480c9c5a
Yash <jewalkaryash@gmail.com>
2023-06-29 09:59:06 +0530

    completed login and signup functionalities

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from todo_app.views import index_view,sign_up_view,sign_in_view
+from todo_app.views import index_view,sign_up_view,sign_in_view,sign_out_view
 
 urlpatterns = [
     path('',index_view,name='index'),
     path('sign-up/',sign_up_view,name='sign_up'),
     path('sign-in/',sign_in_view,name='sign_in'),
+    path('sign-out/',sign_out_view,name='sign_out'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,13 +1,67 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from todo_app.models import User
 # Create your views here.
+from django.contrib.auth.models import auth
+from django.contrib.auth.decorators import login_required
 def index_view(request):
-    user=request.user
+    
     return render(request,'index.html')
 
 
 def sign_up_view(request):
-    return render(request,'signup.html')
+    page_name='signup.html'
+    if request.method == "POST":
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+       
+        if not email:
+            return render(request,page_name,{'error':True,'message':'email is empty'})
+        if not username:
+            return render(request,page_name,{'error':True,'message':'username is empty'})
+        if not password:
+            return render(request,page_name,{'error':True,'message':'password is empty'})
+        if User.objects.filter(username=username).exists():
+             return render(request,page_name,{'error':True,'message':'User already exists !'})
+        if User.objects.filter(email=email).exists():
+             return render(request,page_name,{'error':True,'message':'This email id is associated with any other username !'})
+        if User.objects.filter(username=username).exists():
+             return render(request,page_name,{'error':True,'message':'User already exists !'})
+        User.objects.create_user(username=username,email=email,password=password)
+        user=auth.authenticate(username=username,password=password)
+        if user:
+            auth.login(request,user)
+            return redirect('index')
+        else:
+            return render(request,page_name,{'error':True,'message':'Authentication failed'})
+    else:
+        return render(request,page_name)
 
 
 def sign_in_view(request):
-    return render(request,'signin.html')
+    page_name='signin.html'
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        if not username:
+            return render(request,page_name,{'error':True,'message':'username is empty'})
+        if not password:
+            return render(request,page_name,{'error':True,'message':'password is empty'})
+        user=auth.authenticate(username=username,password=password)
+        if user:
+            auth.login(
+                request,
+                user
+            )
+            return redirect('index')
+        else:
+            return render(request,page_name,{'error':True,'message':'Authentication failed'})
+    else:
+        return render(request,'signin.html')
+
+
+
+@login_required(login_url='sign_in')
+def sign_out_view(request):
+    auth.logout(request)
+    return redirect('index')
